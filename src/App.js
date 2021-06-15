013078a39968740df7146ad70becfeec75b2952e
Sean <writeseanhong@gmail.com>
2021-06-15 14:41:30 -0500

    updated navigation and added grid to main

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
-import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
-import { Menu, MenuItem } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Main from './components/Main';
-import About from './components/About';
-import NotFound from './components/NotFound';
-import Nav from './components/Nav';
-import './App.css';
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import { Menu, MenuItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Main from "./components/Main";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
+import Nav from "./components/Nav";
+import "./App.css";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   appContainer: {
-      height: '100vh',
-      width: '100vw',
+    height: "100vh",
+    width: "100vw",
   },
-}))
+  viewArea: {
+    marginTop: theme.spacing(1),
+  },
+}));
 
 const App = () => {
-
   const [anchorEl, setAnchorEl] = useState(null);
   const classes = useStyles();
   const handleClose = () => {
     setAnchorEl(null);
-  }
+  };
 
   return (
     <div className={classes.appContainer}>
       <Router>
         <Nav openMenu={setAnchorEl} />
-        <Switch>
-          <Route path="/" exact component={Main}/>
-          <Route path="/about" component={About}/>
-          <Route path="*" component={NotFound}/>
-        </Switch>
+        <div className={classes.viewArea}>
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/about" component={About} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
         <Menu
           id="simple-menu"
           anchorEl={anchorEl}
@@ -39,12 +43,20 @@ const App = () => {
           open={Boolean(anchorEl)}
           onClose={handleClose}
         >
-          <MenuItem onClick={handleClose}><Link to="/" className={classes.links}>Hello there</Link></MenuItem>
-          <MenuItem onClick={handleClose}><Link to="/about" className={classes.links}>About</Link></MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link to="/" className={classes.links}>
+              Hello there
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link to="/about" className={classes.links}>
+              About
+            </Link>
+          </MenuItem>
         </Menu>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import vader from '../files/memes/nacho.jpg';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import vader from "../files/memes/nacho.jpg";
 
-const useStyles = makeStyles(theme => ({
-    mainRoot: {
-        margin: 'auto',
-        display: 'inline-block',
-    },
-}))
+const useStyles = makeStyles((theme) => ({
+  imageContainer: {},
+}));
 
 const Main = () => {
-    const classes = useStyles();
-    return(
-        <div>
-            <div className={classes.mainRoot}>
-                <img src={vader} alt="Vader" />
-            </div>
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Grid container justify="center">
+      <Grid item className={classes.imageContainer}>
+        <img src={vader} alt="Jarth Mader" />
+      </Grid>
+    </Grid>
+  );
+};
 
-export default Main
+export default Main;
