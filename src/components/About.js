22f9d928cedbff3e1fd1d0dd0975d66566c7eff4
Sean <writeseanhong@gmail.com>
2021-06-15 15:11:31 -0500

    updated test

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
-    return(
-        <h1>This is about me</h1>
-    )
-}
+  return <div>About me?</div>;
+};
 
-export default Main
+export default Main;
