f3655bd53366632ecf246fd8ca101536affac344
Sean <writeseanhong@gmail.com>
2021-06-19 02:19:58 -0500

    added octonaut

--- src/components/Fart.js
+++ src/components/Fart.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { IconButton, makeStyles } from "@material-ui/core";
+import { IconButton, makeStyles, Grid } from "@material-ui/core";
 import AirlineSeatLegroomReducedIcon from "@material-ui/icons/AirlineSeatLegroomReduced";
+import FaceIcon from "@material-ui/icons/Face";
 import Fart1 from "../files/sounds/fart1.mp3";
 import Fart2 from "../files/sounds/fart2.mp3";
 import Fart3 from "../files/sounds/fart3.mp3";
 import Fart4 from "../files/sounds/fart4.mp3";
 import Fart5 from "../files/sounds/fart5.mp3";
 import Fart6 from "../files/sounds/fart6.mp3";
+import OctoAlert from "../files/sounds/octo_alert.mp3";
 import { randomIntGenerator } from "../utils/funky";
 
-const useStyles = makeStyles((theme) => ({
-  cakeContainer: {
-    width: theme.spacing(50),
-    height: theme.spacing(50),
-  },
-}));
+const useStyles = makeStyles((theme) => ({}));
 
 const Fart = () => {
   const classes = useStyles();
@@ -25,12 +22,28 @@ const Fart = () => {
     const thunder = new Audio(fart);
     thunder.play();
   };
+  const handleOctoAlert = () => {
+    const alert = new Audio(OctoAlert);
+    alert.play();
+  };
   return (
-    <div className={classes.cakeContainer}>
-      <IconButton aria-label="delete" onClick={handleFart}>
-        <AirlineSeatLegroomReducedIcon style={{ fontSize: "400px" }} />
-      </IconButton>
-    </div>
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      className={classes.cakeContainer}
+    >
+      <Grid item>
+        <IconButton aria-label="delete" onClick={handleFart}>
+          <AirlineSeatLegroomReducedIcon style={{ fontSize: "200px" }} />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton aria-label="delete" onClick={handleOctoAlert}>
+          <FaceIcon style={{ fontSize: "200px" }} />
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 };
 
