df82e6cf8046af9b69ffef66e84df4f5ee1b6c18
Sean <writeseanhong@gmail.com>
2021-07-24 12:14:23 -0500

    a bunch of changes too much to list

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -3,6 +3,8 @@ import { Grid, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PetsIcon from "@material-ui/icons/Pets";
 import axios from "axios";
+import grass from "../files/images/grass1.png";
+import doggy from "../files/images/doberman.png";
 
 const useStyles = makeStyles((theme) => ({
   dogContainer: {
@@ -11,6 +13,30 @@ const useStyles = makeStyles((theme) => ({
   },
   dogImg: {
     maxWidth: "100%",
+  },
+  getDogButton: {
+    display: "inline-block",
+    position: "absolute",
+    top: "64px",
+    left: "200px",
+  },
+  grass: {
+    display: "inline-block",
+    position: "absolute",
+    bottom: "0",
+    "& img": {
+      width: "100%",
+    },
+  },
+  doberman: {
+    maxWidth: "200px",
+    "& img": {
+      width: "100%",
+    },
+    margin: theme.spacing(0, 3),
+  },
+  flip: {
+    transform: "rotateY(180deg)",
   },
 }));
 
@@ -35,21 +61,26 @@ const Dog = () => {
 
   console.log("howday", dogData);
   return (
-    <Grid container justify="center" direction="column" alignItems="center">
-      {dogData !== undefined && (
-        <>
-          <Grid item>
-            <IconButton aria-label="delete" onClick={getDog}>
-              <PetsIcon style={{ fontSize: "50px" }} />
-              <p>Click Me for Another!</p>
-            </IconButton>
-          </Grid>
-          <br />
-          <Grid item className={classes.dogContainer}>
-            <img src={dogData} alt="doggo" className={classes.dogImg} />
-          </Grid>
-        </>
-      )}
+    <Grid container justify="center" direction="row" alignItems="center">
+      <Grid item className={`${classes.doberman} ${classes.flip}`}>
+        <img src={doggy} alt="" />
+      </Grid>
+      <Grid item>
+        <IconButton
+          aria-label="delete"
+          onClick={getDog}
+          className={classes.getDogButton}
+        >
+          <PetsIcon style={{ fontSize: "50px" }} />
+        </IconButton>
+        <img src={dogData} alt="doggo" className={classes.dogImg} />
+      </Grid>
+      <Grid item className={classes.doberman}>
+        <img src={doggy} alt="" />
+      </Grid>
+      <div className={classes.grass}>
+        <img src={grass} alt="some grass" />
+      </div>
     </Grid>
   );
 };
---
+++ src/components/Glitch.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../styles/glitch.scss";
+
+const Glitch = () => {
+  return (
+    <div className="glitch-container">
+      <div className="glitch" data-text="Welcome">
+        Welcome
+      </div>
+      <div className="jobTitle"></div>
+    </div>
+  );
+};
+
+export default Glitch;
